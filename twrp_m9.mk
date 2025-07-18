d27aa0091f52f3d4815b7ae88ea85aa4bc1b37e3
KSN <redawewrq12@gmail.com>
2025-07-18 16:10:58 +0300

    Update twrp_m9.mk

--- twrp_m9.mk
+++ twrp_m9.mk
@@ -1,25 +1,16 @@
-#
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Inherit from t6100a_wifi device
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/lenovo/m9/device.mk)
 
-PRODUCT_DEVICE := t6100a_wifi
+PRODUCT_DEVICE := m9
 PRODUCT_NAME := twrp_m9
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Tab M9
-PRODUCT_MANUFACTURER := lenovo
-
-PRODUCT_GMS_CLIENTID_BASE := android-lenovo
-
+PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_BOARD := mt6768
